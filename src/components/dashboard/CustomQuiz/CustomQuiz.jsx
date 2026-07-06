fb163057d0571f407c2b0016440da0afdf5e5cdb
Radian06 <aksen05aksen@gmail.com>
2026-07-06 15:52:52 +0900

    feat(#4): 실시간 맞춤 퀴즈 패널 구현

--- src/components/dashboard/CustomQuiz/CustomQuiz.jsx
+++ src/components/dashboard/CustomQuiz/CustomQuiz.jsx
@@ -1,7 +1,12 @@
 function CustomQuiz() {
     return (
-        <div className="panel-base justify-center w-[37.86vw] h-[23.24vh]">
+        <div className="panel-base justify-center w-[37.86vw] h-[23.24vh] gap-[1.94vh]">
             <div className="panel-title">실시간 맞춤 퀴즈</div>
+            <div className="flex flex-col items-center gap-[0.46vh] text-[1.67vh]">
+                <p>감지된 취약점과 관련된 최적화 퀴즈가 준비되었습니다.</p>
+                <p>실시간 피드백으로 보안 역량을 강화하세요.</p>
+            </div>
+            <div className="flex justify-center items-center w-[7.86vw] h-[4.35vh] bg-purple500 text-[1.85vh] rounded-[1.04vw] font-bold text-gray200 cursor-pointer hover:bg-purple400 hover:text-gray700">퀴즈 풀기</div>
         </div>
     );
 }
