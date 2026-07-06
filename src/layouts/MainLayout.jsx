2125e34e758fbf08ff7364069c1dddf829f455a0
Radian06 <aksen05aksen@gmail.com>
2026-07-06 16:06:48 +0900

    feat(#4): IDE 미연결 상태의 Outlet 제작

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,17 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 import SideNav from '../components/SideNav';
 import IdeStatus from '../components/IdeStatus';
+import IdeDisconnected from '../components/IdeDisconnected'; 
 
 export default function MainLayout() {
+  const [isConnected, setIsConnected] = useState(false); 
+
   return (
     <div className="flex justify-between w-[95.6vw] h-[92.6vh] my-[3.7vh] mx-[2.1vw]">
       <SideNav />
 
       <div className="flex flex-col justify-between w-[77.3vw] h-full">
-        <IdeStatus isConnected={true} />
+        <IdeStatus isConnected={isConnected} />
 
         <div className="flex w-[77.3vw] h-[83.33vh]">
-          <Outlet />
+          {isConnected ? <Outlet /> : <IdeDisconnected />}
         </div>
       </div>
     </div>
