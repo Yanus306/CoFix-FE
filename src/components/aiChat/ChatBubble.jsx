f94685d4ddccbc1ea5329b497b5310e86912759b
Radian06 <aksen05aksen@gmail.com>
2026-07-20 16:58:26 +0900

    fix(#24): ChatBubble 경고 수정

--- src/components/aiChat/ChatBubble.jsx
+++ src/components/aiChat/ChatBubble.jsx
@@ -7,7 +7,7 @@ export default function ChatBubble({ role, message }) {
     <div className={`flex items-start w-full mb-[2.5vh] ${isUser ? "justify-end" : "justify-start"}`}>
       
       {!isUser && (
-        <img src={Logo} alt="AI" className="flex-shrink-0 w-[4.5vh] h-[4.5vh] mr-[0.8vw] rounded-full" />
+        <img src={Logo} alt="AI" className="shrink-0 w-[4.5vh] h-[4.5vh] mr-[0.8vw] rounded-full" />
       )}
 
       <div
@@ -18,7 +18,7 @@ export default function ChatBubble({ role, message }) {
             : "bg-gray800-50 border-[0.09vh] border-white-5 rounded-[1.04vw] rounded-tl-none" 
         }`}
       >
-        <span className="whitespace-pre-wrap break-words">{message}</span>
+        <span className="whitespace-pre-wrap wrap-break-words">{message}</span>
       </div>
       
     </div>
