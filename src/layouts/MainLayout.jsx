471167b29a406d9866971b16f6e61675010143c6
Radian06 <aksen05aksen@gmail.com>
2026-07-21 17:26:50 +0900

    WTP(#30): 머지 전 충돌 방지를 위한 IDE 연동 상태 변경

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -5,9 +5,8 @@ import IdeStatus from '../components/IdeStatus';
 import IdeDisconnected from '../components/IdeDisconnected';
 import NotLoggedIn from '../components/NotLoggedIn';
 
-// 💡 1. 부모(App.jsx)로부터 isLoggedIn뿐만 아니라 모달을 열어주는 onOpenLogin도 함께 받아옵니다!
 export default function MainLayout({ isLoggedIn, onOpenLogin }) {
-  const [isConnected, setIsConnected] = useState(false); 
+  const [isConnected, setIsConnected] = useState(true); 
 
   return (
     <div className="flex justify-between w-[95.6vw] h-[92.6vh] my-[3.7vh] mx-[2.1vw]">
@@ -20,10 +19,8 @@ export default function MainLayout({ isLoggedIn, onOpenLogin }) {
           {!isConnected ? (
             <IdeDisconnected />
           ) : !isLoggedIn ? (
-            /* 💡 2. 로그인이 안 되어있을 때는 NotLoggedIn 컴포넌트를 보여주고 로그인 버튼 핸들러를 연결합니다. */
             <NotLoggedIn onOpenLogin={onOpenLogin} /> 
           ) : (
-            /* 💡 3. 로그인이 완료(true)되면 주소창(/dashboard 등)에 맞는 컴포넌트(Dashboard)가 Outlet 자리에 착 뜹니다! */
             <Outlet />      
           )}
         </div>
