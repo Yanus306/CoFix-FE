b42b4eea76a30e72d6cd0935a944cd2e0a3330be
Radian06 <aksen05aksen@gmail.com>
2026-07-22 16:34:17 +0900

    Revert "Feat/33 ide code"

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,21 +1,10 @@
-import { useLocation, useNavigate, Outlet } from "react-router-dom";
-import { useEffect } from "react";
-import SideNav from "../components/SideNav/SideNav";
-import IdeStatus from "../components/IdeStatus";
-import IdeDisconnected from "../components/IdeDisconnected";
-import NotLoggedIn from "../components/NotLoggedIn";
+import { Outlet } from 'react-router-dom';
+import SideNav from '../components/SideNav/SideNav';
+import IdeStatus from '../components/IdeStatus';
+import IdeDisconnected from '../components/IdeDisconnected';
+import NotLoggedIn from '../components/NotLoggedIn';
 
-export default function MainLayout({
-  isLoggedIn,
-  isConnected,
-  isWaitingForIde,
-  onOpenLogin,
-}) {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const publicPaths = ["/ide-code"];
-  const isPublicPath = publicPaths.includes(location.pathname);
+export default function MainLayout({ isLoggedIn, isConnected, isWaitingForIde, onOpenLogin }) {
 
   return (
     <div className="flex justify-between w-[95.6vw] h-[92.6vh] my-[3.7vh] mx-[2.1vw]">
@@ -27,10 +16,10 @@ export default function MainLayout({
         <div className="flex w-[77.3vw] h-[83.33vh]">
           {isWaitingForIde ? (
             <IdeDisconnected />
-          ) : (!isLoggedIn && !isPublicPath) ? ( 
-            <NotLoggedIn onOpenLogin={onOpenLogin} />
+          ) : !isLoggedIn ? (
+            <NotLoggedIn onOpenLogin={onOpenLogin} /> 
           ) : (
-            <Outlet />
+            <Outlet />      
           )}
         </div>
       </div>
