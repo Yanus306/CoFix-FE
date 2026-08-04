7968ff7410b939e271e4cc58e19d06b05253acef
Radian06 <aksen05aksen@gmail.com>
2026-08-04 14:31:33 +0900

    feat(#57): Report 개발자 역량 툴팁 적용

--- src/components/report/SkillRadar/SkillRadar.jsx
+++ src/components/report/SkillRadar/SkillRadar.jsx
@@ -1,11 +1,36 @@
 import RadarChartCore from "./RadarChartCore";
 
 const defaultEmptyData = [
-  { subject: "시간 복잡도 최적화", score: 1, fullMark: 100 },
-  { subject: "코드 가독성", score: 1, fullMark: 100 },
-  { subject: "클린코드 준수 여부", score: 1, fullMark: 100 },
-  { subject: "알고리즘 선택의 적절성", score: 1, fullMark: 100 },
-  { subject: "문제 해결력", score: 1, fullMark: 100 },
+  {
+    subject: "시간 복잡도 최적화",
+    score: 1,
+    fullMark: 100,
+    description: "연산 감소 및 실행 시간 단축을 통한 효율적인 코드 작성",
+  },
+  {
+    subject: "코드 가독성",
+    score: 1,
+    fullMark: 100,
+    description: "직관적인 네이밍과 명확한 구조를 통한 가독성 향상",
+  },
+  {
+    subject: "클린코드 준수 여부",
+    score: 1,
+    fullMark: 100,
+    description: "중복 코드 제거와 단일 책임 원칙 준수를 통한 유지보수성",
+  },
+  {
+    subject: "알고리즘 선택의 적절성",
+    score: 1,
+    fullMark: 100,
+    description: "문제 상황과 제약 조건에 부합하는 최적의 알고리즘 선택",
+  },
+  {
+    subject: "문제 해결력",
+    score: 1,
+    fullMark: 100,
+    description: "로직 오류 및 예외 상황에 대한 논리적 분석과 해결책 도출",
+  },
 ];
 
 function SkillRadar({ data }) {
@@ -14,7 +39,7 @@ function SkillRadar({ data }) {
   return (
     <div className="panel-base justify-center w-full h-[39.26vh]">
       <div className="panel-title mt-[4vh]">개발자 핵심 역량 분석</div>
-      
+
       <RadarChartCore data={chartData} />
     </div>
   );
