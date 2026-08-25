62a936a87e4dcbb6ed24a8277e9fa5c4844babce
Radian06 <aksen05aksen@gmail.com>
2026-08-25 13:28:12 +0900

    style: report 레이더 그래프 타이틀 변경

--- src/components/report/SkillRadar/SkillRadar.jsx
+++ src/components/report/SkillRadar/SkillRadar.jsx
@@ -38,7 +38,7 @@ function SkillRadar({ data }) {
 
   return (
     <div className="panel-base justify-center w-full h-[39.26vh]">
-      <div className="panel-title mt-[4vh]">개발자 핵심 역량 분석</div>
+      <div className="panel-title mt-[4vh]">취약점 개선 역량 분석</div>
 
       <RadarChartCore data={chartData} />
     </div>
